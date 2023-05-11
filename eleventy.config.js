21b1b52d4f8c65fadbb661b10340b55e9bf04363
Joshua Coleman <jlcolema@icloud.com>
2023-05-11 18:16:02 +0000

    Add configurations for navigation paths, dates, CSS, and service workers.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,18 @@
+const path = require("path");
+
+// Service Worker
+
+const pluginPWA = require("eleventy-plugin-pwa-v2");
+
+// Dates
+
+const { DateTime } = require("luxon");
+
+// Clean CSS Plugin
+// Source: https://www.11ty.dev/docs/quicktips/inline-css/
+
+const CleanCSS = require("clean-css");
+
 // Navigation Plugin
 // Source: https://www.11ty.dev/docs/plugins/navigation/
 
@@ -19,6 +34,31 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
 
 module.exports = function(eleventyConfig) {
+
+	eleventyConfig.addFilter("getbase", function (value) {
+		const { dir } = path.parse(value);
+		return dir + "/";
+	});
+
+	// Service Worker
+
+	eleventyConfig.addPlugin(pluginPWA);
+
+	// Dates
+
+	eleventyConfig.addFilter("postDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+	});
+
+	eleventyConfig.addFilter("isoDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toISODate();
+	});
+
+	// Inline Minified CSS
+
+	eleventyConfig.addFilter("cssmin", function(code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
 
 	// Navigation
 
@@ -46,7 +86,17 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({
 		"./assets/": "/assets/",
 		"./media/": "/media/",
-		"./node_modules/prismjs/themes/prism-okaidia.css": "/assets/css/prism-okaidia.css"
+		"./node_modules/prismjs/themes/prism-okaidia.css": "/assets/css/prism-okaidia.css",
+		"./content/manifest.json": "./"
+	});
+
+	// ---------------------------
+	// Custom Filtering
+	// ---------------------------
+
+	eleventyConfig.addCollection("selectedWork", function(collectionApi) {
+		// Display only Selected Work
+		return collectionApi.getFilteredByTags("project", "selected");
 	});
 
 	// ---------------------------
